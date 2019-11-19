c56229ecf586e7d207fbb0b0062f330408e20fc8
korol <cx.korol@gmail.com>
2019-11-19 16:29:21 +0300

    feat: add url validation

---
+++ src/validation.js
@@ -0,0 +1,64 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { watch } from 'melanke-watchjs';
+import validator from 'validator';
+
+const checkInput = (url) => {
+  if (!validator.isURL(url)) {
+    return 'Not valid URL';
+  }
+  return false;
+};
+
+export default () => {
+  const state = {
+    urlForm: {
+      state: 'empty',
+      message: '',
+    },
+  };
+
+  const formElement = document.getElementById('inputForm');
+  const urlInput = document.getElementById('addURL');
+  const invalidFeedback = formElement.querySelector('.invalid-feedback');
+  const submitBtn = document.getElementById('submitButton');
+
+  watch(state, 'urlForm', () => {
+    invalidFeedback.textContent = '';
+    urlInput.classList.remove('is-invalid');
+    urlInput.classList.remove('is-valid');
+    urlInput.disabled = false;
+    submitBtn.disabled = false;
+
+    switch (state.urlForm.state) {
+      case 'empty':
+        submitBtn.disabled = true;
+        urlInput.value = '';
+        break;
+      case 'invalid':
+        urlInput.classList.add('is-invalid');
+        submitBtn.disabled = true;
+        invalidFeedback.textContent = state.urlForm.message;
+        break;
+      case 'valid':
+        urlInput.classList.add('is-valid');
+        break;
+      default:
+    }
+  });
+
+  urlInput.addEventListener('keyup', (e) => {
+    const url = e.target.value;
+    if (url === '') {
+      state.urlForm.state = 'empty';
+    }
+
+    const errUrl = checkInput(url);
+    if (errUrl) {
+      state.urlForm.state = 'invalid';
+      state.urlForm.message = errUrl;
+    } else {
+      state.urlForm.state = 'valid';
+    }
+  });
+};
